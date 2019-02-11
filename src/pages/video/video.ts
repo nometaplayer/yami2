c399f5b30f96dd04b61aaf3c7c19af5d57a32761
Marlon95 <marlon.rabocelli@hotmail.fr>
2019-02-11 09:16:23 +0100

    video

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
-import { PhotoViewerOriginal } from '@ionic-native/photo-viewer';
+
 
 
 @Component({
@@ -9,18 +9,16 @@ import { PhotoViewerOriginal } from '@ionic-native/photo-viewer';
   templateUrl: 'video.html',
 })
 export class VideoPage {
-  img: string;
+  
+  galleryType='grille';
+  value: string;
   images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
   
 
   constructor(public navCtrl: NavController,
-              public menuCtrl: MenuController,
-              private photoViewer: PhotoViewerOriginal) {
+              public menuCtrl: MenuController) {
   }
   onToggleMenu() {
     this.menuCtrl.open();
   }
-  /* lookImg(value){
-    this.photoViewer.show(value,'', {share: true});
-  } */
 }
